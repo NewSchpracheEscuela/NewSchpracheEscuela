da10f28df8477accfdeb6ee59e0c9424af0bb290
Andrusiok <angrey1996@yandex.ru>
2017-03-15 17:18:46 +0300

    Added Get method

--- src/main/java/PL/UserController.java
+++ src/main/java/PL/UserController.java
@@ -3,22 +3,30 @@ import java.sql.*;
 import DAL.User;
 import DAL.UserRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by alexb on 14-Mar-17.
  */
 @Controller
 public class UserController {
+
+
     @RequestMapping(value = "/index",method = RequestMethod.GET)
-    public String Index() throws ClassNotFoundException, SQLException {
+    public ModelAndView Index(ModelMap model) throws ClassNotFoundException, SQLException {
         UserRepository userRepository = new UserRepository();
         Iterable<User> users = userRepository.GetAll();
         for (User user:
              users) {
             System.out.println(user.login);
+            //request.setAttribute("user", user.login);
+
         }
-        return "index";
+        model.addAttribute("users", users);
+        return new ModelAndView("users", "users", users);
     }
+
 }
