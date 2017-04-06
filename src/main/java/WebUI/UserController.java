67ca64a4cafbb3f7ca996ff42d025132c2c8676a
Alex Budnitsky <alex.budnitsky1997@gmail.com>
2017-04-06 22:36:51 +0300

    add angular scripts

--- src/main/java/WebUI/UserController.java
+++ src/main/java/WebUI/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
 
 /**
  * Created by alexb on 14-Mar-17.
@@ -15,12 +16,17 @@ import org.springframework.web.servlet.ModelAndView;
 public class UserController {
 
 
-    @RequestMapping(value = "/index",method = RequestMethod.GET)
-    public ModelAndView Index(ModelMap model) throws ClassNotFoundException, SQLException {
+    @RequestMapping(value = "/users",method = RequestMethod.GET)
+    public ModelAndView Users(ModelMap model) throws ClassNotFoundException, SQLException {
         UserRepository userRepository = new UserRepository();
         Iterable<User> users = userRepository.GetAll();
         model.addAttribute("users", users);
         return new ModelAndView("users", "users", users);
+    }
+
+    @RequestMapping(value = "/index",method = RequestMethod.GET)
+    public ModelAndView AngularIndex(){
+        return new ModelAndView("index");
     }
 
 }
