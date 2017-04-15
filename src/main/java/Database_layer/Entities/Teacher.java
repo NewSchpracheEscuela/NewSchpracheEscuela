793ddbb5564c7fb1297977a1a3d534334de1c0ea
Alex Budnitsky <alex.budnitsky1997@gmail.com>
2017-04-15 23:28:20 +0300

    add teacher repository

---
+++ src/main/java/Database_layer/Entities/Teacher.java
@@ -0,0 +1,15 @@
+package Database_layer.Entities;
+
+/**
+ * Created by alexb on 15-Apr-17.
+ */
+public class Teacher {
+    private int id;
+    private int user_id;
+
+    public int getId(){return id;}
+    public int getUser_id(){return user_id;}
+
+    public void setId(int newId){id = newId;}
+    public void setUser_id(int newUser_id){user_id = newUser_id;}
+}
---
+++ src/tests/Database_layer/Repositories/TeacherRepositoryTest.java
@@ -0,0 +1,56 @@
+package Database_layer.Repositories;
+
+import Database_layer.Entities.ControlPoint;
+import Database_layer.Entities.Group;
+import Database_layer.Entities.Teacher;
+import Database_layer.IRepository;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by alexb on 15-Apr-17.
+ */
+public class TeacherRepositoryTest {
+    private static java.sql.Connection connection;
+    private static Statement statement;
+    private IRepository<Teacher> repository;
+    @Before
+    public void setUp()throws Exception{
+        repository = new TeacherRepository();
+    }
+    @Test
+    public void getAll() throws Exception {
+        ArrayList<Teacher> teachers = (ArrayList<Teacher>) repository.GetAll();
+    }
+
+    @Test
+    public void get() throws Exception {
+        Teacher teacher = (Teacher) repository.Get(3);
+    }
+
+    @Test
+    public void add() throws Exception {
+        Teacher teacher = new Teacher();
+        teacher.setUser_id(3);
+        repository.Add(teacher);
+    }
+
+    @Test
+    public void delete() throws Exception {
+        repository.Delete(4);
+    }
+
+    @Test
+    public void update() throws Exception {
+        Teacher teacher = new Teacher();
+        teacher.setUser_id(3);
+        repository.Update(3,teacher);
+    }
+
+}
