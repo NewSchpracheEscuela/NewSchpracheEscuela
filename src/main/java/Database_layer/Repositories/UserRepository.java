b481b2e5d787aa0755f5e34dc29b9e95fad04e84
Alex Budnitsky <alex.budnitsky1997@gmail.com>
2017-04-17 18:06:15 +0300

    edit .gitignore file

--- src/main/java/Database_layer/Repositories/UserRepository.java
+++ src/main/java/Database_layer/Repositories/UserRepository.java
@@ -20,7 +20,7 @@ public class UserRepository implements IRepository<User> {
         try{
             Class.forName("com.mysql.jdbc.Driver");
 
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/database_nse","root","1234");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/database_nse","root","admin");
         }
             catch (Exception e){System.out.println(e);}
     }
--- src/main/java/WebUI/CourseController.java
+++ src/main/java/WebUI/CourseController.java
@@ -2,10 +2,6 @@ package WebUI;
 
 import org.springframework.stereotype.Controller;
 
-/**
- * Created by angre on 15.04.2017.
- */
-
 @Controller
 public class CourseController {
 
